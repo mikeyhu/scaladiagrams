63b1be2c1e426ef7c28fb9a8a67b20a6a076f133
Casper devboxmatsumoto.springer-sbm.com <devboxmatsumoto.springer-sbm.com@springer.com>
2012-08-22 14:59:54 +0100

    Added IGNORED, CASE, OBJECT and a test for an actual file.

--- src/main/scala/net/invalidkeyword/scaladiagrams/ScalaSourceParser.scala
+++ src/main/scala/net/invalidkeyword/scaladiagrams/ScalaSourceParser.scala
@@ -6,30 +6,49 @@ object ScalaSourceParser extends RegexParsers with RunParser {
 
   override def skipWhitespace = true
   
-  lazy val wordExp = regex("""\w+""".r) ^^ {case word => word}
   
-  val wordClass = "class"
-  val wordTrait = "trait"
-  val wordWith = "with"
-  val wordExtends = "extends"
+
+  val ignored = regex("""\S""".r) ^^ {case ig => IGNORED()}
   
-  def classGroup = wordClass~wordExp~rep(withGroup) ^^ {case pre~name~withs => CLASS(name,withs)}
-  def traitGroup = wordTrait~wordExp ^^ {case pre~name => TRAIT(name)} 
+  val wordExp= regex("""\w+""".r) ^^ {case word => word}
+  
+  val brackets = regex("""\([^\)]*\)""".r)
+  
+  val wordClass = "class "
+  val wordCase = "case class "
+  val wordTrait = "trait "
+  val wordObject = "object "
+  val wordWith = "with "
+  val wordExtends = "extends "
+  
+  def classGroup : Parser[EXP] = wordClass~wordExp~opt(brackets)~rep(withGroup) ^^ {case pre~name~brackets~withs => CLASS(name,withs)}
+  def traitGroup : Parser[EXP]= wordTrait~wordExp~rep(withGroup) ^^ {case pre~name~withs => TRAIT(name,withs)} 
+  def objectGroup : Parser[EXP]= wordObject~wordExp~rep(withGroup) ^^ {case pre~name~withs => OBJECT(name,withs)} 
+  def caseGroup : Parser[EXP]= wordCase~wordExp~opt(brackets)~rep(withGroup) ^^ {case pre~name~brackets~withs => CASE(name,withs)} 
+  
   def withGroup = (wordExtends|wordWith)~wordExp ^^ {case pre~name => WITH(name)}
   
-  def root = (classGroup|traitGroup)
+  def parsable : Parser[List[EXP]] = rep(caseGroup|classGroup|traitGroup|objectGroup|ignored)
   
-  type RootType = EXP
+  def root = parsable 
+  
+  type RootType = List[EXP]
+  
+  def filter(matches : List[EXP]) = matches.filter(r => r!=IGNORED())
+  
 }
 
 trait RunParser {
   this: RegexParsers =>
     type RootType
   def root: Parser[RootType]
-  def run(in: String): ParseResult[RootType] = parseAll(root, in) 
+  def run(in: String): ParseResult[RootType] = parseAll(root, in)
 }
 
+case class CASE(name : String, withs : {}) extends EXP
+case class OBJECT(name : String, withs : {}) extends EXP
 case class CLASS(name : String, withs : {}) extends EXP
-case class TRAIT(name : String) extends EXP
+case class TRAIT(name : String, withs : {}) extends EXP
 case class WITH(name : String) extends EXP
+case class IGNORED() extends EXP
 abstract class EXP
--- src/test/scala/net/invalidkeyword/scaladiagrams/ScalaSourceParserTests.scala
+++ src/test/scala/net/invalidkeyword/scaladiagrams/ScalaSourceParserTests.scala
@@ -9,36 +9,78 @@ class ScalaSourceParserTests extends Spec with ShouldMatchers  {
     it("should parse a class") {
       val result = ScalaSourceParser.run("class bob")
       result.successful should be(true)
-      result.get should be(CLASS("bob",List()))
+      ScalaSourceParser.filter(result.get) should be(List(CLASS("bob",List())))
     }
     
     it("should parse a trait") {
       val result = ScalaSourceParser.run("trait Cat")
       result.successful should be(true)
-      result.get should be(TRAIT("Cat"))
+      ScalaSourceParser.filter(result.get) should be(List(TRAIT("Cat",List())))
     }
     
     it("should fail to parse some other text") {
       val result = ScalaSourceParser.run("bob is a cat")
-      result.successful should be(false)
+      ScalaSourceParser.filter(result.get) should be(List())
     }   
     
     it("should parse a class with an with") {
       val result = ScalaSourceParser.run("class bob with bill")
       result.successful should be(true)
-      result.get should be(CLASS("bob",List(WITH("bill"))))
+      ScalaSourceParser.filter(result.get) should be(List(CLASS("bob",List(WITH("bill")))))
     }
         
     it("should parse a class with an extends") {
       val result = ScalaSourceParser.run("class bob extends bill")
       result.successful should be(true)
-      result.get should be(CLASS("bob",List(WITH("bill"))))
+      ScalaSourceParser.filter(result.get) should be(List(CLASS("bob",List(WITH("bill")))))
     }
     
     it("should parse a class with an extends and some withs") {
       val result = ScalaSourceParser.run("class bob extends bill with peter with paul")
       result.successful should be(true)
-      result.get should be(CLASS("bob",List(WITH("bill"),WITH("peter"),WITH("paul"))))
+      ScalaSourceParser.filter(result.get) should be(List(CLASS("bob",List(WITH("bill"),WITH("peter"),WITH("paul")))))
     }
+    
+    it("should parse a class after some other text") {
+      val result = ScalaSourceParser.run("this is a test class bob")
+      result.successful should be(true)
+      ScalaSourceParser.filter(result.get) should be(List(CLASS("bob",List())))
+    }
+    
+    it("should parse a class before some other text") {
+      val result = ScalaSourceParser.run("class bob and some more stuff")
+      result.successful should be(true)
+      ScalaSourceParser.filter(result.get) should be(List(CLASS("bob",List())))
+    }
+    
+    it("should parse a class with some other text") {
+      val result = ScalaSourceParser.run("abc some class bob and some trait bill with peter more stuff")
+      result.successful should be(true)
+      ScalaSourceParser.filter(result.get) should be(List( CLASS("bob",List()),TRAIT("bill",List(WITH("peter"))) ))
+    }
+    
+    it("should parse a case class with params") {
+      val result = ScalaSourceParser.run("case class bob (abc : String) extends bill")
+      result.successful should be(true)
+      ScalaSourceParser.filter(result.get) should be(List(CASE("bob",List(WITH("bill")))))
+    }
+    
+    it("should parse a source file") {
+      val input = scala.io.Source.fromFile("/home/casper/scaladiagrams/src/main/scala/net/invalidkeyword/scaladiagrams/ScalaSourceParser.scala").mkString
+      val result = ScalaSourceParser.run(input)
+      result.successful should be(true)
+      ScalaSourceParser.filter(result.get) should be(List(
+          OBJECT("ScalaSourceParser",List(WITH("RegexParsers"),WITH("RunParser"))),
+          TRAIT("RunParser",List()),
+          CASE("CASE",List(WITH("EXP"))),
+          CASE("OBJECT",List(WITH("EXP"))),
+          CASE("CLASS",List(WITH("EXP"))),
+          CASE("TRAIT",List(WITH("EXP"))),
+          CASE("WITH",List(WITH("EXP"))),
+          CASE("IGNORED",List(WITH("EXP"))),
+          CLASS("EXP",List())
+      ))
+    }
+    
   }
 }
